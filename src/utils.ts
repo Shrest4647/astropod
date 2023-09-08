82e76e95ef80b1220d1d4be2d7c43a12a66717b2
Sunil Shrestha <shrest464760+github@gmail.com>
2023-09-09 00:10:37 +0545

    Adding null condition

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
 export function staticPath(path: string) {
-    const BASE_URL = import.meta.env.BASE_URL;
+    const BASE_URL = import.meta.env.BASE_URL ?? '/astropod';
     return `${BASE_URL}${path}`
 }
